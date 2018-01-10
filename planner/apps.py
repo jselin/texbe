6c606fbb3ed0907f98a1eb619e8abe364357b87a
Jari Selin <jari@selin.fi>
2018-01-10 22:01:53 +0200

    trying to sole symlink problem

--- planner/apps.py
+++ planner/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
+import os
 
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        return False
 
 class PlannerConfig(AppConfig):
     name = 'planner'
+    if(!get_env_variable):
+        path = '/opt/python/current/app/planner'
+
