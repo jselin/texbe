78fc21ea84e6c32315bc272388587734db30e65a
Jari Selin <jari@selin.fi>
2018-01-13 19:19:08 +0200

    Deployment works

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
- 
+
+echo Environment
+echo DB_NAME $DB_NAME
+echo DB_USERNAME $DB_USERNAME
+echo DB_HOSTNAME $DB_HOSTNAME
+
 echo Collect static files
 python manage.py collectstatic --clear --noinput 
 python manage.py collectstatic --noinput  
 
 echo Starting migrations
-./wait-for.sh $DB_HOSTNAME:$DB_PORT --timeout=10 -- python3 manage.py migrate
+#./wait-for.sh $DB_HOSTNAME:$DB_PORT --timeout=10 -- python3 manage.py migrate
+python3 manage.py migrate
 
 echo Starting nginx
 rm /etc/nginx/nginx.conf
