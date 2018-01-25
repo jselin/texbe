9e0fc8074177873c5b4c07545e247c495097da45
Jari Selin <jari@selin.fi>
2018-01-25 17:33:40 +0200

    Started test cases

--- planner/tests.py
+++ planner/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+class FooBarTestCase(TestCase):
+    def setUp(sefl):
+        self.x = 0
+    def test_foo(self):
+        """Test foo"""
+        self.assertEqual(self.x, 0)
+    def test_bar(self):
+        """Test bar"""
+        self.assertEqual(self.x, 1)
